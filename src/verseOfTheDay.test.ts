a8c382184af0d55f97241c10ba73e4c05415fd19
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-09-15 23:11:26 -0500

    Switch to dayjs because timezones are hard

--- src/verseOfTheDay.test.ts
+++ src/verseOfTheDay.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test, vi } from "vitest";
 
-import { getDayOfYear, getVerseReferenceOfTheDay } from "./verseOfTheDay";
+import { getVerseReferenceOfTheDay } from "./verseOfTheDay";
 
 vi.mock("./data/verseOfTheDayList.json", () => {
   return {
@@ -8,31 +8,20 @@ vi.mock("./data/verseOfTheDayList.json", () => {
   };
 });
 
-describe("getDayOfYear()", () => {
-  test("should return the expected day of the year", () => {
-    expect(getDayOfYear(new Date("2024-01-01T11:00:00+05:00"))).toBe(1);
-    expect(getDayOfYear(new Date("2024-01-20T11:00:00+05:00"))).toBe(20);
-    expect(getDayOfYear(new Date("2024-02-01T11:00:00+05:00"))).toBe(32);
-    expect(getDayOfYear(new Date("2024-03-01T11:00:00+05:00"))).toBe(61);
-    // 2024 is a leap year
-    expect(getDayOfYear(new Date("2024-12-31T11:00:00+05:00"))).toBe(366);
-  });
-});
-
 describe("getVerseReferenceOfTheDay()", () => {
   test("should return a verse reference in the collection", () => {
-    expect(
-      getVerseReferenceOfTheDay(new Date("2024-01-01T11:00:00+05:00")),
-    ).toBe("Genesis 1:1");
-    expect(
-      getVerseReferenceOfTheDay(new Date("2024-01-02T11:00:00+05:00")),
-    ).toBe("John 3:16");
-    expect(
-      getVerseReferenceOfTheDay(new Date("2024-01-03T11:00:00+05:00")),
-    ).toBe("Psalm 23:1");
+    expect(getVerseReferenceOfTheDay("2024-01-01T11:00:00+05:00")).toBe(
+      "Genesis 1:1",
+    );
+    expect(getVerseReferenceOfTheDay("2024-01-02T11:00:00+05:00")).toBe(
+      "John 3:16",
+    );
+    expect(getVerseReferenceOfTheDay("2024-01-03T11:00:00+05:00")).toBe(
+      "Psalm 23:1",
+    );
     // loop back to the beginning of the collection
-    expect(
-      getVerseReferenceOfTheDay(new Date("2024-01-04T11:00:00+05:00")),
-    ).toBe("Genesis 1:1");
+    expect(getVerseReferenceOfTheDay("2024-01-04T11:00:00+05:00")).toBe(
+      "Genesis 1:1",
+    );
   });
 });
