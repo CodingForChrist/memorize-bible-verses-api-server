74fb134cae072146442c86c93a2d03d8434402c9
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-09-15 23:56:26 -0500

    Return more date information for easier debugging (#23)

--- src/verseOfTheDay.test.ts
+++ src/verseOfTheDay.test.ts
@@ -10,18 +10,26 @@ vi.mock("./data/verseOfTheDayList.json", () => {
 
 describe("getVerseReferenceOfTheDay()", () => {
   test("should return a verse reference in the collection", () => {
-    expect(getVerseReferenceOfTheDay("2024-01-01T11:00:00+05:00")).toBe(
-      "Genesis 1:1",
-    );
-    expect(getVerseReferenceOfTheDay("2024-01-02T11:00:00+05:00")).toBe(
-      "John 3:16",
-    );
-    expect(getVerseReferenceOfTheDay("2024-01-03T11:00:00+05:00")).toBe(
-      "Psalm 23:1",
-    );
+    expect(getVerseReferenceOfTheDay("2024-01-01T00:00:00+05:00")).toEqual({
+      verseReference: "Genesis 1:1",
+      dayOfTheYear: 1,
+      formattedDate: "January 1, 2024 12:00 AM",
+    });
+    expect(getVerseReferenceOfTheDay("2024-01-02T00:00:00+05:00")).toEqual({
+      verseReference: "John 3:16",
+      dayOfTheYear: 2,
+      formattedDate: "January 2, 2024 12:00 AM",
+    });
+    expect(getVerseReferenceOfTheDay("2024-01-03T00:00:00+05:00")).toEqual({
+      verseReference: "Psalm 23:1",
+      dayOfTheYear: 3,
+      formattedDate: "January 3, 2024 12:00 AM",
+    });
     // loop back to the beginning of the collection
-    expect(getVerseReferenceOfTheDay("2024-01-04T11:00:00+05:00")).toBe(
-      "Genesis 1:1",
-    );
+    expect(getVerseReferenceOfTheDay("2024-01-04T00:00:00+05:00")).toEqual({
+      verseReference: "Genesis 1:1",
+      dayOfTheYear: 4,
+      formattedDate: "January 4, 2024 12:00 AM",
+    });
   });
 });
--- src/verseOfTheDay.ts
+++ src/verseOfTheDay.ts
@@ -1,19 +1,38 @@
 import dayjs from "dayjs";
 import dayOfYear from "dayjs/plugin/dayOfYear";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import utc from "dayjs/plugin/utc";
 
 dayjs.extend(dayOfYear);
+dayjs.extend(localizedFormat);
+dayjs.extend(utc);
 
 import verseOfTheDayList from "./data/verseOfTheDayList.json";
 
 export function getVerseReferenceOfTheDay(
   dateISOStringWithTimezoneOffset: string,
 ) {
-  const dayOfYearIndex = dayjs(dateISOStringWithTimezoneOffset).dayOfYear() - 1;
+  const originalTimezone = dateISOStringWithTimezoneOffset.slice(-6);
+  const dayjsDate = dayjs(dateISOStringWithTimezoneOffset).utcOffset(
+    originalTimezone,
+  );
 
-  // TODO: add more verse references to get to 366
-  if (dayOfYearIndex >= verseOfTheDayList.length) {
-    return verseOfTheDayList[dayOfYearIndex - verseOfTheDayList.length];
+  const dayOfTheYear = dayjsDate.dayOfYear();
+  const dayOfTheYearIndex = dayOfTheYear - 1;
+
+  let verseReference: string;
+
+  if (dayOfTheYearIndex < verseOfTheDayList.length) {
+    verseReference = verseOfTheDayList[dayOfTheYearIndex];
+  } else {
+    // TODO: add more verse references to get to 366
+    verseReference =
+      verseOfTheDayList[dayOfTheYearIndex - verseOfTheDayList.length];
   }
 
-  return verseOfTheDayList[dayOfYearIndex];
+  return {
+    verseReference,
+    dayOfTheYear,
+    formattedDate: dayjsDate.format("LLL"),
+  };
 }
