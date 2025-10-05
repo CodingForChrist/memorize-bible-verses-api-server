3abddabd9be91e692e11bdbfd5f3ef6f72663d30
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-10-04 23:31:59 -0500

    Get to 366 verses for Verse of the Day endpoint

--- src/data/verseOfTheDayList.test.ts
+++ src/data/verseOfTheDayList.test.ts
@@ -9,31 +9,66 @@ describe("verseOfTheDayList", () => {
     expect(verseOfTheDayList.length).toBe(new Set(verseOfTheDayList).size);
   });
 
-  test("should spell bible book names correctly", () => {
+  test("should have 366 verses", () => {
+    expect(verseOfTheDayList.length).toBe(366);
+  });
+});
+
+describe.for(verseOfTheDayList)("validate verse %s", (verse) => {
+  test("should spell bible book name correctly", () => {
     const bookNames = ["Psalm", ...books.map(({ name }) => name)];
+    const foundBook = bookNames.find((name) => {
+      return verse.startsWith(name);
+    });
 
-    for (const verse of verseOfTheDayList) {
-      const foundBook = bookNames.find((name) => {
-        return verse.startsWith(name);
-      });
-
-      if (!foundBook) {
-        throw new Error(`Unknown book name for verse "${verse}"`);
-      }
+    if (!foundBook) {
+      throw new Error(`Unknown book name for verse "${verse}"`);
     }
   });
 
   test("should be in expected verse reference format", () => {
-    for (const verse of verseOfTheDayList) {
-      try {
-        parseVerseReferenceIntoParts(verse);
-      } catch (error) {
-        throw new Error(`Invalid verse format for "${verse}", ${error}`);
-      }
+    try {
+      parseVerseReferenceIntoParts(verse);
+    } catch (error) {
+      throw new Error(`Invalid verse format for "${verse}", ${error}`);
     }
   });
 
-  // test("should have 366 verses", () => {
-  //   expect(verseOfTheDayList.length).toBe(366);
-  // });
+  test("should not be longer than 4 verses", () => {
+    const { verseCount } = parseVerseReferenceIntoParts(verse);
+    if (verseCount > 4) {
+      throw new Error(
+        `verse reference range contains too many verses ${verse}`,
+      );
+    }
+  });
 });
+
+// test("should spell bible book names correctly", () => {
+//   const bookNames = ["Psalm", ...books.map(({ name }) => name)];
+
+//   for (const verse of verseOfTheDayList) {
+//     const foundBook = bookNames.find((name) => {
+//       return verse.startsWith(name);
+//     });
+
+//     if (!foundBook) {
+//       throw new Error(`Unknown book name for verse "${verse}"`);
+//     }
+//   }
+// });
+
+// test("should be in expected verse reference format", () => {
+//   for (const verse of verseOfTheDayList) {
+//     try {
+//       parseVerseReferenceIntoParts(verse);
+//     } catch (error) {
+//       throw new Error(`Invalid verse format for "${verse}", ${error}`);
+//     }
+//   }
+// });
+
+//   test("should have 365 verses", () => {
+//     expect(verseOfTheDayList.length).toBe(365);
+//   });
+// });
