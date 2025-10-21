2d50e454273347be884e3430debe7a3d2f0deb2e
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-10-21 09:12:49 -0500

    chore: avoid sync file system operations (#34)

--- src/tasks/checkNodeVersion.ts
+++ src/tasks/checkNodeVersion.ts
@@ -1,29 +1,34 @@
-import { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 import { satisfies } from "semver";
 
 const __dirname = import.meta.dirname;
 
-const minimumNodeVersion = readFileSync(
-  join(__dirname, "../../", ".nvmrc"),
-  "utf-8",
-);
+try {
+  const minimumNodeVersion = await readFile(
+    join(__dirname, "../../", ".nvmrc"),
+    "utf-8",
+  );
 
-const isValidNodeVersion = satisfies(
-  process.version,
-  `>= ${minimumNodeVersion}`,
-);
+  const isValidNodeVersion = satisfies(
+    process.version,
+    `>= ${minimumNodeVersion}`,
+  );
 
-// successfully exit when Node version is valid
-if (isValidNodeVersion) {
-  process.exitCode = 0;
-} else {
-  const output = `
-  ** Invalid Node.js Version **
-  current version: ${process.version}
-  minimum required version: ${minimumNodeVersion}
-  `;
+  // successfully exit when Node version is valid
+  if (isValidNodeVersion) {
+    process.exitCode = 0;
+  } else {
+    const output = `
+      ** Invalid Node.js Version **
+      current version: ${process.version}
+      minimum required version: ${minimumNodeVersion}
+      `;
 
-  console.error(output);
+    console.error(output);
+    process.exitCode = 1;
+  }
+} catch (error) {
+  console.error("Error checking Node.js version:", error);
   process.exitCode = 1;
 }
